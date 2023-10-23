6123d5a8ac9777fc16bdb2790d2c7823cc7163e7
Zedrik <20231000077@feualabang.edu.ph>
2023-10-23 22:15:19 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+#include"source files\credentials.h"
+
+using namespace std;
+
+main()
+{
+    UserCredentials *uc = new UserCredentials();
+    uc->user_credentials();   
+}
---
+++ source files/credentials.h
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<string>
+#include<windows.h>
+#include<unistd.h>
+#include"helloworld.h"
+using namespace std;
+
+class UserCredentials
+{
+    public:
+    void user_credentials()
+    {
+    char response;
+    const string USERNAME="admin";
+	const string PASSWORD="password";
+	string username,password;
+	bool user_validated=false;
+
+	do 
+	{
+			{
+                system("cls");
+				cout<<"Enter username: ";cin>>username;
+                cout<<"Enter password: ";cin>>password;
+                
+                if (username==USERNAME&&password==PASSWORD) //TRUE
+				{
+					cout<<"User credentials are correct, Granting access"<<endl;
+                    Sleep(1000);
+                    //usleep(1000); //FOR UNIX SYSTEMS
+                    system("cls");
+					user_validated=true;
+                    helloworld *hw = new helloworld();
+                    hw->hello_world();
+				}else //FALSE
+				{
+					cout<<"Wrong credentials"<<endl;
+                    cout<<"Exit? (y/n): ";cin>>response;
+                    // Program end task
+                    if (response=='y')
+                    {
+                        return;
+                    }
+                    // User invalid input 
+                    if (response!='n') 
+                    {
+                        cout<<"Invalid input!";
+                        Sleep(1000);
+                        system("cls");
+                    }
+				}
+			}
+	} while (user_validated==false);
+    }
+};
+
