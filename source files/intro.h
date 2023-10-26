ae27863f51e4e8f5634cb8dfe395f4150fa3c411
Zedrik <kurtzedrikdy@gmail.com>
2023-10-26 09:20:06 +0800

    new commit

---
+++ source files/intro.h
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+class intro
+{
+public:
+    void program_intro()
+    {
+    system("cls");
+    cout<<endl;
+    cout<<"\t|-----------|";
+    cout<<"\n\t|  BANKING  |";
+	cout<<"\n\t|  SYSTEM   |";
+    cout<<"\n\t|-----------|"<<endl<<endl;
+	system("pause");
+    }
+};
---
+++ test-files/intro.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+    cout<<endl;
+    cout<<"\t|-----------|";
+    cout<<"\n\t|  BANKING  |";
+	cout<<"\n\t|  SYSTEM   |";
+    cout<<"\n\t|-----------|"<<endl<<endl;
+	system("pause");
+}
